bf706e2c9de0cad3fa586d592bfb44ccbe68ef47
MathyasLefebvre <mathylefebvre@hotmail.com>
2025-05-16 15:54:51 -0400

    ✨ start implementing the exercice backend

--- ProgChess/ProgChess.Server/Dto/ExerciceDto.cs
+++ ProgChess/ProgChess.Server/Dto/ExerciceDto.cs
@@ -2,5 +2,7 @@ namespace ProgChess.Server.Dto;
 
 public class ExerciceDto
 {
+    public required string Situation { get; set; }
     
+    public required string Code { get; set; }
 }
--- ProgChess/ProgChess.Server/Services/Interfaces/IExerciceService.cs
+++ ProgChess/ProgChess.Server/Services/Interfaces/IExerciceService.cs
@@ -1,6 +1,11 @@
+using ProChess.Server.Entities;
+using ProgChess.Server.Dto;
+
 namespace ProgChess.Server.Services;
 
-public interface IExerciceToken
+public interface IExerciceService
 {
-    
+    public Task<Exercice> Create(ExerciceDto request);
+    Task<List<Exercice>?> GetAllExercice();
+    Task<Exercice?> GetById(int id);
 }

26d8bb0e12bb1d9fee5172ebc71fc1d699e196b1
MathyasLefebvre <mathylefebvre@hotmail.com>
2025-05-27 15:40:53 -0400

    ✨keep width on reload

--- ProgChess/progchess.client/src/components/layout/HorizontalResizable.tsx
+++ ProgChess/progchess.client/src/components/layout/HorizontalResizable.tsx
@@ -6,7 +6,13 @@ interface BoxCardProps {
 
 export default function HorizontalResizable({ children }: BoxCardProps) {
   const isResized = useRef(false);
-  const [width, setWidth] = useState(window.innerWidth / 2);
+  const [width, setWidth] = useState(
+    parseInt(localStorage.getItem("leftWidth")!) || window.innerWidth / 2
+  );
+
+  useEffect(() => {
+    localStorage.setItem("leftWidth", width.toString());
+  }, [width]);
 
   useEffect(() => {
     window.addEventListener("mousemove", (e) => {
@@ -25,7 +31,6 @@ export default function HorizontalResizable({ children }: BoxCardProps) {
     <div className="flex">
       <div style={{ width: `${width / 16}rem` }}>{children}</div>
 
-      {/* Resizable element */}
       <div
         onMouseDown={() => {
           isResized.current = true;

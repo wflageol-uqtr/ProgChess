62711829e5b2206e137b2cdc91f6a7565a0c9a23
MathyasLefebvre <mathylefebvre@hotmail.com>
2025-06-09 13:46:06 -0400

    💄add accordion to show test info

--- ProgChess/progchess.client/src/components/sheet/ExecutionSheet.tsx
+++ ProgChess/progchess.client/src/components/sheet/ExecutionSheet.tsx
@@ -1,3 +1,4 @@
+import { AccordionItem } from "@radix-ui/react-accordion";
 import {
   Sheet,
   SheetContent,
@@ -5,12 +6,16 @@ import {
   SheetTitle,
 } from "../../components/ui/sheet";
 import type { TestResult } from "../../utils/type";
+import { Accordion, AccordionContent, AccordionTrigger } from "../ui/accordion";
 
 interface ExecutionSheetProps {
   open: boolean;
   onOpenChange: (open: boolean) => void;
   error?: string;
   testResult?: TestResult[];
+}
+{
+  /* Regarder type d'erreur Mardi avec william */
 }
 
 export default function ExecutionSheet({
@@ -38,20 +43,33 @@ export default function ExecutionSheet({
               </div>
             </div>
           ) : testResult?.length ? (
-            testResult.map((test, index) => (
-              <div
-                key={index}
-                className={`p-3 rounded-lg flex items-center justify-between ${
-                  test.success ? "bg-green-600" : "bg-red-600"
-                }`}
-              >
-                {/* Regarder type d'erreur Mardi avec william */}
-                <span>{test.testName}</span>
-                <span>{test.success ? "✅ Réussi" : "❌ Échoué"}</span>
-              </div>
-            ))
+            <Accordion className="mx-2 space-y-3" type="single" collapsible>
+              {testResult.map((test, index) => (
+                <AccordionItem className="rounded-none" value={`item-${index}`}>
+                  <AccordionTrigger
+                    className={`flex cursor-pointer items-center justify-between px-4 py-3 text-white font-medium transition rounded-none ${
+                      test.success
+                        ? "bg-green-500 hover:bg-green-600"
+                        : "bg-red-500 hover:bg-red-600"
+                    }`}
+                  >
+                    {test.testName}
+                  </AccordionTrigger>
+                  <AccordionContent
+                    className={`px-4 py-3 ${
+                      test.success
+                        ? "bg-green-100 text-green-500"
+                        : "bg-red-100 text-red-500"
+                    }`}
+                  >
+                    <p>{test.actual || "Aucun information disponible"}</p>
+                    <p>{test.expected}</p>
+                  </AccordionContent>
+                </AccordionItem>
+              ))}
+            </Accordion>
           ) : (
-            <div>Aucun résultat</div>
+            <div className="flex justify-center">Aucun résultat</div>
           )}
         </div>
       </SheetContent>

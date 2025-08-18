2fa6b76b78c93f7466b5764701a21e53afe9e720
MathyasLefebvre <mathylefebvre@hotmail.com>
2025-08-18 15:12:18 -0400

    📝 add documentation

--- ProgChess/progchess.client/src/pages/score/ListScore.tsx
+++ ProgChess/progchess.client/src/pages/score/ListScore.tsx
@@ -28,7 +28,7 @@ export default function ListScore() {
       <div className="h-min-screen flex flex-col px-4 space-y-2 md:space-y-4 w-full mt-4">
         <div className="flex justify-between w-full">
           <h2 className="text-2xl font-semibold text-white">
-            Liste des scores
+            Liste des résultats
           </h2>
           <Link to="/admin/score/create">
             <Button
